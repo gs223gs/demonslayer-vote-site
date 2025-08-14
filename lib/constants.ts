a714d65a18cc379006f21bd7e228f32d369d9dc8
T.Miura <mtaiga0213@icloud.com>
2025-08-15 00:27:21 +0900

    feat: 興行収入をファイルで管理 全てのページを一律で管理するため

---
+++ lib/constants.ts
@@ -0,0 +1,11 @@
+export const BOX_OFFICE_AMOUNT = '200億';
+export const BOX_OFFICE_AMOUNT_EN = '20 billion';
+
+export const COOKIE_NAMES = {
+  VOTED_CHARACTER: 'voted_character',
+  LANGUAGE: 'language',
+} as const;
+
+export const COOKIE_MAX_AGE = 60 * 60 * 24 * 90; // 3 months in seconds
+
+export const CACHE_REVALIDATE_TIME = 60; // 1 minute in seconds
