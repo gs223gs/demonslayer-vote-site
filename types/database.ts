c53cd0fdc39109eb84afaff85b4369abae335b72
T.Miura <mtaiga0213@icloud.com>
2025-08-15 01:18:07 +0900

    feat: 型定義にカテゴリを実装

--- types/database.ts
+++ types/database.ts
@@ -8,7 +8,9 @@ export interface Character {
   id: string;
   name_ja: string;
   name_en: string;
+  category: 'demon' | 'corps' | 'civilian';
   display_order: number;
+  //TODO: Add image_url when images are ready
 }
 
 export interface VoteCount {
